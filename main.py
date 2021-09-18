fe0dad644f2cd8a9cfba251c7d796360109bd497
guisalmeida <guilherme.dasilva@linx3.com>
2021-09-17 21:02:17 -0300

    feat: add loop task for classes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+import requests
+import json
+from datetime import datetime
+
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,12 +13,13 @@ token = os.getenv('TOKEN')
 matricula = os.getenv('MATRICULA')
 password = os.getenv('SENHA')
 
-bot = Bot('!')
+bot = commands.Bot('!')
 
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    current_time.start()
     await bot.change_presence(
         status=discord.Status.idle,
         activity=discord.Game('Assembly no FreeBSD')
@@ -37,6 +42,36 @@ async def on_message(message):
 async def send_hello(ctx):
 
     await ctx.send('Eu amo!')
+
+
+@tasks.loop(minutes=1)
+async def current_time():
+    channel = bot.get_channel(877701880472547328)
+
+    now = datetime.now()
+    now_time = now.strftime('%H:%M:00')
+    now_date = now.strftime('%Y-%m-%d')
+
+    if '19:10:00' in now_time or '20:45:00' in now_time:
+
+        # Login na api para pegar token
+        url_login = f'https://www.ies.edu.br/includes/head.asp' \
+            f'?action=logar&matricula={matricula}&senha={password}'
+
+        login = requests.get(url_login)
+        data_login = json.loads(login.text)
+        user_token = data_login['token']
+
+        # Bate na api para pegar dados das aulas
+        url_classes = f'https://suafaculdade.com.br' \
+            f'/api/servicos/Aluno/ObterAulaOnline/{user_token}'
+
+        data_classes = requests.get(url_classes)
+        classes = json.loads(data_classes.text)
+
+        for cls in classes:
+            if now_date in cls['DataAula'] and now_time in cls['DataAula']:
+                await channel.send(json.dumps(cls, sort_keys=False, indent=4))
 
 
 bot.run(token)
